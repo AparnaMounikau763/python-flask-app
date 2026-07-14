292a399974114bf4261ffa539a51b6787ebe0ab2
Mounika <aparnamounikaanupoju@gmail.com>
2026-07-14 22:44:38 +0530

    first commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,7 @@ import pytest
 
 from app.app import app
 
+
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
@@ -12,33 +13,30 @@ def client():
 
 
 def test_home(client):
-
     response = client.get("/")
 
     assert response.status_code == 200
 
-    data = json.loads(response.data)
+    data = response.get_json()
 
     assert data["application"] == "Python Flask Demo"
 
 
 def test_health(client):
-
     response = client.get("/health")
 
     assert response.status_code == 200
 
-    data = json.loads(response.data)
+    data = response.get_json()
 
     assert data["status"] == "Healthy"
 
 
 def test_add(client):
-
     response = client.get("/add/10/20")
 
     assert response.status_code == 200
 
-    data = json.loads(response.data)
+    data = response.get_json()
 
     assert data["result"] == 30
